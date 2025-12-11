7bda47b5f29fc1a65d53b6b9d3e6ff61c7b00377
Abdul Hanif Athhar <hanifconsultans@gmail.com>
2025-12-11 16:45:16 +0700

    update

---
+++ app/api/rekap/route.tsx
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const response = await fetch(
+      "http://103.196.116.97:81/api/statistics/overall",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer dokdwri2i4mnhhfhjj4dadqo4i`,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Gagal fetch API eksternal");
+    }
+
+    const data = await response.json();
+
+    // Return data mentah dari API eksternal
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error("❌ Error:", error);
+    return NextResponse.json(
+      { error: "Gagal mengambil data dari server eksternal" },
+      { status: 500 }
+    );
+  }
+}
